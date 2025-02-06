8b00c66260adaae1429a295d119c1571b4006689
Ali-agela <aliagela09@gmail.com>
2025-02-07 00:48:32 +0200

    first commit

---
+++ lib/helper/constant.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const blue1 = Color(0xFF163172);
+const blue2 = Color(0xff1E56A0);
+const blue3 = Color(0xFF525CEB);
+const blue4 = Color(0xFF3E92CC);
+const green = Color(0xFF208B3A);
+const red = Color(0xFFCB0000);
